d2b9fd93fc9e71f28613e8ba6d54b25fd7c4e20d
Jacky Zheng <jzheng1126@yahoo.com>
2018-06-06 19:58:50 -0700

    Added function to grab player object by lastname. Added JSON file to only store top 10 players in draft.

--- NBA Draft Prospect/app.js
+++ NBA Draft Prospect/app.js
@@ -1,7 +1,45 @@
-function loadpic(name){
+function loadpic(name) {
 	var elem = document.createElement("img");
 
 	elem.setAttribute("src", "images/"+name+".jpg");
 	elem.setAttribute("alt", name);
 	document.getElement.ById(name).appendChild(elem);
 }
+
+/*
+*	Returns top 10 draft picks as a JSON object.
+*/
+
+function loadFile() {
+	var fs = require("fs");
+	var content = fs.readFileSync("player10.json");
+	return JSON.parse(content);
+}
+
+/*
+*	Pass in output from loadFile <-- it's a JSON object. Pass in last name.
+*	Returns Player object with attributes:
+*		points,
+*		rebounds,
+*		steals,
+*		blocks,
+*		minutes,
+*		assists
+*/
+
+function findPlayer(lastName, jsonObject) {
+	var player_dict = jsonObject[lastName];
+	var player = {
+		points: player_dict["PPG"],
+		rebounds: parseFloat(player_dict["ORB"]) + parseFloat(player_dict["DRB"]),
+		steals: player_dict["SPG"],
+		blocks: player_dict["BPG"],
+		minutes: player_dict["MPG"],
+		assists: player_dict["APG"]
+	};
+	return player;
+}
+
+var jsonObject = loadFile();
+var doncic = findPlayer("doncic", jsonObject);
+console.log(doncic);
