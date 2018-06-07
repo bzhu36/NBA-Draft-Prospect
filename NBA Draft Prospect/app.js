6cbe1343c79a2f94e52f11720092e5586aa496bc
dennismfong <dfong2014@gmail.com>
2018-06-06 20:57:14 -0700

    Add javascript functions for loading player graphs

--- NBA Draft Prospect/app.js
+++ NBA Draft Prospect/app.js
@@ -1,3 +1,235 @@
+var PLAYERJSON = 
+{
+	"ayton": {
+            "Name": "DeAndre Ayton",
+            "Team": "University of Arizona", 
+            "GP": "35", 
+            "MPG": "33.5", 
+            "FGM": "7.9", 
+            "FGA": "12.9", 
+            "FGP": ".612", 
+            "TPM": "0.3", 
+            "TPA": "1.0", 
+            "TPP": ".343", 
+            "FTM": "4.0", 
+            "FTA": "5.5", 
+            "FTP": ".733", 
+            "ORB": "3.4", 
+            "DRB": "8.2", 
+            "RPG": "11.6", 
+            "APG": "1.6", 
+            "SPG": "0.6", 
+            "BPG": "1.9", 
+            "PPG": "20.1"
+        }, 
+        "bagley": {
+        	"Name": "Marvin Bagley III",
+            "Team": "DUKE", 
+            "GP": "33", 
+            "MPG": "33.9", 
+            "FGM": "8.2", 
+            "FGA": "13.3", 
+            "FGP": ".614", 
+            "TPM": "0.7", 
+            "TPA": "1.8", 
+            "TPP": ".397", 
+            "FTM": "4.0", 
+            "FTA": "6.3", 
+            "FTP": ".627", 
+            "ORB": "4.0", 
+            "DRB": "7.1", 
+            "RPG": "11.1", 
+            "APG": "1.5", 
+            "SPG": "0.8", 
+            "BPG": "0.9", 
+            "PPG": "21.0"
+        }, 
+        "bamba": {
+        	"Name": "Mohamed Bamba",
+            "Team": "University of Texas", 
+            "GP": "30", 
+            "MPG": "30.2", 
+            "FGM": "4.9", 
+            "FGA": "9.0", 
+            "FGP": ".541", 
+            "TPM": "0.5", 
+            "TPA": "1.7", 
+            "TPP": ".275", 
+            "FTM": "2.7", 
+            "FTA": "4.0", 
+            "FTP": ".681", 
+            "ORB": "3.2", 
+            "DRB": "7.3", 
+            "RPG": "10.5", 
+            "APG": "0.5", 
+            "SPG": "0.8", 
+            "BPG": "3.6", 
+            "PPG": "12.9"
+        }, 
+        "jackson": {
+            "Name": "Jaren Jackson",
+            "Team": "Michigan State University", 
+            "GP": "35", 
+            "MPG": "21.8", 
+            "FGM": "3.4", 
+            "FGA": "6.6", 
+            "FGP": ".513", 
+            "TPM": "1.1", 
+            "TPA": "2.7", 
+            "TPP": ".396", 
+            "FTM": "3.0", 
+            "FTA": "3.8", 
+            "FTP": ".797", 
+            "ORB": "1.5", 
+            "DRB": "4.3", 
+            "RPG": "5.8", 
+            "APG": "1.1", 
+            "SPG": "0.6", 
+            "BPG": "3.0", 
+            "PPG": "10.9"
+        }, 
+        "doncic": {
+        	"Name": "Luka Doncic",
+            "Team": "Real Madrid", 
+            "GP": "33", 
+            "MPG": "25.9", 
+            "FGM": "4.7", 
+            "FGA": "10.5", 
+            "FGP": ".451", 
+            "TPM": "1.7", 
+            "TPA": "5.2", 
+            "TPP": ".329", 
+            "FTM": "4.8", 
+            "FTA": "5.9", 
+            "FTP": ".816", 
+            "ORB": "0.8", 
+            "DRB": "4.0", 
+            "RPG": "4.8", 
+            "APG": "4.3", 
+            "SPG": "1.1", 
+            "BPG": "0.3", 
+            "PPG": "16.0"
+        },
+        "porter": {
+        	"Name": "Michael Porter",
+            "Team": "Missouri State University", 
+            "GP": "3", 
+            "MPG": "17.7", 
+            "FGM": "3.3", 
+            "FGA": "10.0", 
+            "FGP": ".333", 
+            "TPM": "1.0", 
+            "TPA": "3.3", 
+            "TPP": ".300", 
+            "FTM": "2.3", 
+            "FTA": "3.0", 
+            "FTP": ".778", 
+            "ORB": "1.3", 
+            "DRB": "5.3", 
+            "RPG": "6.7", 
+            "APG": "0.3", 
+            "SPG": "1.0", 
+            "BPG": "0.3", 
+            "PPG": "10.0"
+        },  
+        "bridges": {
+        	"Name": "Mikal Bridges",
+            "Team": "Villanova University", 
+            "GP": "40", 
+            "MPG": "32.1", 
+            "FGM": "6.1", 
+            "FGA": "11.9", 
+            "FGP": ".514", 
+            "TPM": "2.6", 
+            "TPA": "6.0", 
+            "TPP": ".435", 
+            "FTM": "2.9", 
+            "FTA": "3.4", 
+            "FTP": ".851", 
+            "ORB": "1.4", 
+            "DRB": "3.9", 
+            "RPG": "5.3", 
+            "APG": "1.9", 
+            "SPG": "1.5", 
+            "BPG": "1.1", 
+            "PPG": "17.6"
+        },
+        "young": {
+        	"Name": "Trae Young",
+            "Team": "Oklahoma University", 
+            "GP": "32", 
+            "MPG": "35.4", 
+            "FGM": "8.2", 
+            "FGA": "19.3", 
+            "FGP": ".423", 
+            "TPM": "3.7", 
+            "TPA": "10.2", 
+            "TPP": ".361", 
+            "FTM": "7.4", 
+            "FTA": "8.6", 
+            "FTP": ".861", 
+            "ORB": "0.4", 
+            "DRB": "3.5", 
+            "RPG": "3.9", 
+            "APG": "8.7", 
+            "SPG": "1.7", 
+            "BPG": "0.2", 
+            "PPG": "27.4"
+        }, 
+        "carter": {
+        	"Name": "Wendall Carter Jr.",
+            "Team": "Duke University", 
+            "GP": "37", 
+            "MPG": "26.9", 
+            "FGM": "4.8", 
+            "FGA": "8.6", 
+            "FGP": ".561", 
+            "TPM": "0.5", 
+            "TPA": "1.2", 
+            "TPP": ".413", 
+            "FTM": "3.4", 
+            "FTA": "4.5", 
+            "FTP": ".738", 
+            "ORB": "2.9", 
+            "DRB": "6.1", 
+            "RPG": "9.1", 
+            "APG": "2.0", 
+            "SPG": "0.8", 
+            "BPG": "2.1", 
+            "PPG": "13.5"
+           },
+           "alexander": {
+           	"Name": "Shai Gilgeous-Alexander",
+            "Team": "University of Kentucky", 
+            "GP": "37", 
+            "MPG": "33.7", 
+            "FGM": "4.9", 
+            "FGA": "10.2", 
+            "FGP": ".485", 
+            "TPM": "0.6", 
+            "TPA": "1.5", 
+            "TPP": ".404", 
+            "FTM": "3.9", 
+            "FTA": "4.7", 
+            "FTP": ".822", 
+            "ORB": "0.9", 
+            "DRB": "3.2", 
+            "RPG": "4.1", 
+            "APG": "5.1", 
+            "SPG": "1.6", 
+            "BPG": "0.5", 
+            "PPG": "14.4"
+        }  
+}
+
+function loadAllPlayers() {
+	console.log(PLAYERJSON)
+	for (var player in PLAYERJSON) {
+		loadPlayer(player, PLAYERJSON);
+	}
+}
+
+
 function loadpic(name) {
 	var elem = document.createElement("img");
 
@@ -10,10 +242,34 @@ function loadpic(name) {
 *	Returns top 10 draft picks as a JSON object.
 */
 
-function loadFile() {
-	var fs = require("fs");
-	var content = fs.readFileSync("player10.json");
-	return JSON.parse(content);
+
+function loadPlayer(player) {
+	chartName = player + '_chart'
+	playerObject = findPlayer(player, PLAYERJSON)
+	var ctx = document.getElementById(chartName).getContext('2d');
+	var chart = new Chart(ctx, {
+	    // The type of chart we want to create
+	    type: 'bar',
+
+	    // The data for our dataset
+	    data: {
+	      labels: ["mins", "pts", "rebs", "asts", "blocks", "steals"],
+	      datasets: [
+	        {
+	          backgroundColor: ['rgb(255, 99, 132)', 'rgb(255, 99, 0)', 'rgb(0, 99, 132)', 'rgb(255, 0, 132)', 'rgb(0, 99, 0)', 'rgb(0, 0, 132)'],
+	          borderColor: ['rgb(255, 99, 132)', 'rgb(255, 99, 0)', 'rgb(0, 99, 132)', 'rgb(255, 0, 132)', 'rgb(0, 99, 0)', 'rgb(0, 0, 132)'],
+	          data: [playerObject['minutes'], playerObject['points'], playerObject['rebounds'], playerObject['assists'], playerObject['blocks'], playerObject['steals']],
+	        }
+	      ]
+	    },
+
+	    // Configuration options go here
+	    options: {
+	    	legend: {
+	    		display:false
+	    	}
+	    }
+	});
 }
 
 /*
@@ -40,6 +296,6 @@ function findPlayer(lastName, jsonObject) {
 	return player;
 }
 
-var jsonObject = loadFile();
-var doncic = findPlayer("doncic", jsonObject);
-console.log(doncic);
+loadAllPlayers();
+
+
